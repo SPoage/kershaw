b0bfbd1ca629342be387e9eda6a9569b0ec103c7
Shane Poage <shane.poage@gmail.com>
2015-05-04 02:40:26 -0500

    Updated to use object.__getattribute__ and object.__setattr__

--- kershaw/lang.py
+++ kershaw/lang.py
@@ -3,7 +3,7 @@ from contextlib import contextmanager
 
 @contextmanager
 def ephemeral_value(obj, property_name, value):
-    prev_property_value = getattr(obj, property_name)
-    setattr(obj, property_name, value)
+    prev_property_value = object.__getattribute__(obj, property_name)
+    object.__setattr__(obj, property_name, value)
     yield
-    setattr(obj, property_name, prev_property_value)
+    object.__setattr__(obj, property_name, prev_property_value)
